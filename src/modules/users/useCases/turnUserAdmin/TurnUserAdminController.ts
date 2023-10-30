bc99e413ba805d03da0e918123bedf8490365a96
Hugo Arcanjo <hfarcanjo@gmail.com>
2023-10-30 16:29:28 -0300

    add turn user admin controller and use case

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -3,10 +3,19 @@ import { Request, Response } from "express";
 import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
 
 class TurnUserAdminController {
-  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
+  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) { }
 
   handle(request: Request, response: Response): Response {
     // Complete aqui
+    const { user } = request.body;
+
+    try {
+      this.turnUserAdminUseCase.execute(user);
+
+      return response.status(200).send();
+    } catch (error) {
+      return response.status(404).json({ error: "User Dont Exists" });
+    }
   }
 }
 
