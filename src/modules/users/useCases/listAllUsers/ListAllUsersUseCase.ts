ce31011933b53475866f5b5046549a27139d0bae
Hugo Arcanjo <hfarcanjo@gmail.com>
2023-10-30 17:46:28 -0300

    add list all users controller and use case

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,23 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
     // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user.admin) {
+      throw new Error("Mensagem do erro");
+    }
+
+    if (!user) {
+      throw new Error("Mensagem do erro");
+    }
+
+    const users = this.usersRepository.list();
+
+    return users;
   }
 }
 
